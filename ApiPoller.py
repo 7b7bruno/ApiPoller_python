92cc3bd5fe44e1a51833ae3648b2d6f0231f988c
Bruno <7b7bruno@gmail.com>
2025-11-11 16:30:12 +0200

    Fixing network loss detection

--- ApiPoller.py
+++ ApiPoller.py
@@ -124,7 +124,7 @@ class ConfigManager:
         """Support 'in' operator."""
         return key in self.config or key in self.defaults
 
-VERSION = "V0.3.1"
+VERSION = "V0.3.2"
 
 last_successful_request = time.time()
 last_successful_command_request = time.time()
