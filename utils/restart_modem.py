ed4b2e551b69904589ef70142c2635a3e9b51ca4
Bruno <7b7bruno@gmail.com>
2025-11-14 12:40:29 +0200

    bugfix

--- utils/restart_modem.py
+++ utils/restart_modem.py
@@ -3,7 +3,7 @@ import subprocess
 import time
 
 DEFAULT_WAIT_TIME = 60
-RESTART_DELAY = 5
+DEFAULT_RESTART_DELAY = 5
 ON_COMMAND = ['sudo', 'uhubctl', '-l', '1-1', '-p', '3', '-a', 'on']
 OFF_COMMAND = ['sudo', 'uhubctl', '-l', '1-1', '-p', '3', '-a', 'off']
 
@@ -23,20 +23,23 @@ def restartModem(waitTime, restartDelay):
 if __name__ == '__main__':
     if len(sys.argv) == 1:
         waitTime = DEFAULT_WAIT_TIME
+        restartDelay = DEFAULT_RESTART_DELAY
     elif len(sys.argv) == 2:
         waitTime = int(sys.argv[1])
+        restartDelay = DEFAULT_RESTART_DELAY
     elif len(sys.argv) == 3:
+        waitTime = int(sys.argv[1])
         restartDelay = int(sys.argv[2])
     else:
         waitTime = DEFAULT_WAIT_TIME
-        restartDelay = RESTART_DELAY
+        restartDelay = DEFAULT_RESTART_DELAY
         print(f"Incorrect argument syntax! Should pe python restart_modem.py [wait_time] [restart_delay]")
 
     if waitTime == DEFAULT_WAIT_TIME:
         print(f"Using default wait time: {waitTime}")
     else:
         print(f"Using custom wait time: {waitTime}")
-    if restartDelay == RESTART_DELAY:
+    if restartDelay == DEFAULT_RESTART_DELAY:
         print(f"Using default restart delay: {restartDelay}")
     else:
         print(f"Using custom restart delay: {restartDelay}")
