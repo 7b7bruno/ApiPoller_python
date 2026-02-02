5c58d477a759a884930ed22131cbf6d4814791ff
Bruno <7b7bruno@gmail.com>
2026-02-02 18:30:37 +0200

    changed version number to v0.3.4

--- ApiPoller.py
+++ ApiPoller.py
@@ -126,7 +126,7 @@ class ConfigManager:
         """Support 'in' operator."""
         return key in self.config or key in self.defaults
 
-VERSION = "V0.3.3"
+VERSION = "V0.3.4"
 
 last_successful_request = time.time()
 last_successful_command_request = time.time()
